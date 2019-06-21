7aea03997c24b6a797b77b79cdc563e3ca80cb5e
Kyesha Francis <kyeshafrancis@Kyeshas-Air.YWCA-MIAMI.LOCAL>
2019-06-21 14:05:21 -0400

    changed server

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const app = express();
-app.listen(5000, () => {
-  console.log(`Example app listening on port 5000`)
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.use(express.static(__dirname +`public`));
